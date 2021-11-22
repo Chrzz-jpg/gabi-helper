9c03db1b0e216674ab1323d53db9cc2cd11eb5d2
Chris <chrisroos14@gmail.com>
2021-11-22 16:19:32 -0300

    Base criada

--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { Col, Container, Row } from "react-bootstrap";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const mespeak = require("mespeak");
+mespeak.loadConfig(require("mespeak/src/mespeak_config.json"));
+mespeak.loadVoice(require("mespeak/voices/pt.json"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default class App extends Component {
+  frase = [];
+  state = {
+    layoutName: "default",
+    default: {
+      default: [
+        "1 2 3 4 5 6 7 8 9 0 {bksp}",
+        "Q W E R T U I O P",
+        "A S D F G H J K L ?",
+        "Z X C V B N M , . {enter}",
+        "{space}",
+      ],
+    },
+    input: "",
+    theme: "dark",
+  };
+
+  onChange = (input) => {
+    this.setState({ input });
+
+    let aux = 0;
+    if (input !== "{space}" && input !== "{enter}") {
+      this.frase[aux] = `{${input}}`;
+      aux++;
+    }
+  };
+
+  onKeyPress = (button) => {
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+
+    if (button === "{enter}") {
+      mespeak.speak(this.frase);
+
+      this.keyboardRef.clearInput();
+      this.frase = [];
+    }
+  };
+
+  handleShift = () => {
+    const layoutName = this.state.layoutName;
+
+    this.setState({
+      layoutName: layoutName === "default" ? "shift" : "default",
+    });
+  };
+
+  onChangeInput = (event) => {
+    const input = event.target.value;
+    this.setState({ input });
+    this.keyboardRef.setInput(input);
+  };
+
+  render() {
+
+
+    return (
+      <Container>
+        <Row>
+          <input
+            value={this.state.input}
+            placeholder={
+              "Utilize o teclado para montar sua frase e aperte 'Enter' para falar"
+            }
+            onChange={this.onChangeInput}
+          />
+        </Row>
+
+        <Row>
+          <Col md={12}>
+            <Keyboard
+              keyboardRef={(r) => (this.keyboardRef = r)}
+              theme={`hg-theme-default ${this.state.theme}`}
+              layoutName={this.state.layoutName}
+              layout={this.state.default}
+              dark
+              onChange={this.onChange}
+              onKeyPress={this.onKeyPress}
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
